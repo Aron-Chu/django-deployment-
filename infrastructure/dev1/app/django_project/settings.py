2aba01f259ecc4b8b57439fb048d3f0f7c0d4faa
Aron-Chu <aroncloudchu@gmail.com>
2021-07-14 20:33:26 +0000

    added docker healthcheck and ignore file

--- infrastructure/dev1/app/django_project/settings.py
+++ infrastructure/dev1/app/django_project/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'exhlfdat&vfum(-34*c2uroi(($ww(yo$9pv98=e6p^gl(-eoj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 ALLOWED_HOSTS = ["*"]
 
